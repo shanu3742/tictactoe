8d22175e7f0e39d64378d7422eb764d1aba36edd
shanu3742 <krshanu.95@gmail.com>
2021-05-20 06:21:36 -0700

    board added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Board from './component/Board';
 
 const App = () => {
   return (
-    <>
-      <h1>Welcome to React Parcel Micro App!</h1>
-      <p>Hard to get more minimal than this React app.</p>
-    </>
+    <div>
+      <h1>Tic Tac Tac</h1>
+      <Board />
+    </div>
   );
 };
 export default App;
