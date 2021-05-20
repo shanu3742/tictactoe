a8589a289736b7b90ea89ad7dc24c072b3967fe8
shanu3742 <krshanu.95@gmail.com>
2021-05-20 12:58:35 -0700

    style sheet is added to program

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Board from './component/Board';
+import './style/root.scss';
 
 const App = () => {
   return (
-    <div>
+    <div className="app">
       <h1>Tic Tac Tac</h1>
       <Board />
     </div>
