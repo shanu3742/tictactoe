b52111450dd272dcc28a599a6497208d648ea5d8
shanu3742 <krshanu.95@gmail.com>
2021-05-22 04:54:36 -0700

    winner announce

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { useState } from 'react';
 import Board from './component/Board';
+import { calculateWinner } from './helpers';
 import './style/root.scss';
 
+const Firstplayer = prompt('Enter Firstplayer name');
+const Secondplayer = prompt('Enter Secondplayer name');
 const App = () => {
   const [board, setboard] = useState(Array(9).fill(null));
 
   // set next clicked
 
   const [isXNext, setXNext] = useState(false);
+
+  // winner calculate
+
+  const winner = calculateWinner(board);
+  let message = null;
+  // console.log(winner);
+  if (winner === 'o') {
+    message = `${Firstplayer} win`;
+  } else if (winner === 'x') {
+    message = `${Secondplayer} win`;
+  }
 
   const handleSquareClick = poistion => {
     if (board[poistion]) {
@@ -26,7 +40,7 @@ const App = () => {
   return (
     <div className="app">
       <h1>Tic Tac Toe</h1>
-      <h2>Message</h2>
+      <h2>{message}</h2>
       <Board board={board} handleSquareClick={handleSquareClick} />
     </div>
   );
